1dfc5cbfb1c9968642aab916f83cdf93b39ddfa2
ibkam <ibrahimkamau254@gmail.com>
2023-11-15 11:26:41 -0500

    updates

--- shell_utils.c
+++ shell_utils.c
@@ -1,23 +1,21 @@
 #include "shell.h"
 
 /**
- * Auth: Emma Udeji
- * 		 Pericles Adjovi
- *
  * Description:
  * the extended functions for main.c
  */
 
 
-/** parse_command - determines the type of the command
+/**
+ * parse_command - determines the type of the command
  * @command: command to be parsed
  *
  * Return: constant representing the type of the command
  * Description -
- * 		 EXTERNAL_COMMAND (1) represents commands like /bin/ls
- *		 INTERNAL_COMMAND (2) represents commands like exit, env
- *		 PATH_COMMAND (3) represents commands found in the PATH like ls
- *		 INVALID_COMMAND (-1) represents invalid commands
+ * EXTERNAL_COMMAND (1) represents commands like /bin/ls
+ * INTERNAL_COMMAND (2) represents commands like exit, env
+ * PATH_COMMAND (3) represents commands found in the PATH like ls
+ * INVALID_COMMAND (-1) represents invalid commands
  */
 
 int parse_command(char *command)
